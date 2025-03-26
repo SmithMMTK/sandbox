ecc6c6fec5899d317306bff1c086dca653963a22
SmithMMTK <smithm@hotmail.co.th>
2025-03-26 11:00:01 +0700

    update nodejs to 20

--- scripts/workload_scripts.sh
+++ scripts/workload_scripts.sh
@@ -1,5 +1,5 @@
 hostname && uptime
 sudo apt update -y 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo apt install npm -y
