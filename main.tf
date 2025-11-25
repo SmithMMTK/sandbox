d70160591abb4c0d48c130b30eb0059e71100909
SmithMMTK <smithm@hotmail.co.th>
2025-11-25 09:28:21 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,10 @@ resource "random_pet" "rg_name" {
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
+  
+  tags = {
+    "SecurityControl" = "Ignore"
+  }
 }
 
 output "RESOURCE_GROUP_NAME" {
